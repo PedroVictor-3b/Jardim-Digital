61b184733eb85365894aa8b9689f4161205b5742
Pedro Victor <pedrovictordev3b@gmail.com>
2025-09-14 18:13:00 -0300

    Adicionado icons no tab.routes.js. O projeto ainda continua sem o stack para o cadastro de planta

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StyleSheet, Text, View } from 'react-native';
 import Routes from './src/navigation';
 
 export default function App() {
@@ -7,11 +6,3 @@ export default function App() {
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/navigation/tab.routes.js
+++ src/navigation/tab.routes.js
@@ -1,5 +1,8 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
+import { Feather } from "@expo/vector-icons" //biblioteca de incones
+import { Ionicons } from "@expo/vector-icons"; //biblioteca de incones
+
 //importação das telas
 import HomeScreen from "../screens/HomeScreen";
 import PlantListScreen from "../screens/PlantListScreen";
@@ -7,12 +10,32 @@ import CategoriesScreen from "../screens/CategoriesScreen";
 
 const Tab = createBottomTabNavigator()
 
-export default function TabRoutes(){
-    return(
+export default function TabRoutes() {
+    return (
         <Tab.Navigator>
-            <Tab.Screen name = "Home" component={HomeScreen}/>
-            <Tab.Screen name = "Plantas" component={PlantListScreen}/>
-            <Tab.Screen name = "Categorias" component={CategoriesScreen}/>
+            <Tab.Screen 
+                name="Home" 
+                component={HomeScreen} 
+                options={{
+                    tabBarIcon: ({color, size}) => <Feather name="home" color={color} size={size} />
+                }}    
+            />
+
+            <Tab.Screen 
+                name="Plantas" 
+                component={PlantListScreen} 
+                options={{
+                    tabBarIcon: ({color, size}) => <Ionicons name="leaf" color={color} size={size} />
+                }} 
+            />
+
+            <Tab.Screen 
+                name="Categorias" 
+                component={CategoriesScreen} 
+                options={{
+                    tabBarIcon: ({color, size}) => <Feather name="align-justify" color={color} size={size} />
+                }} 
+            />
         </Tab.Navigator>
     )
 }
